c0d46ffffb2283f75dadc83429144afc3afeac77
李嘉诚 <eremli@mapxus.com>
2026-04-03 17:18:34 +0800

    优化 item view

--- app/src/main/java/com/mapxus/uisdkshowcase/ui/component/ItemCard.kt
+++ app/src/main/java/com/mapxus/uisdkshowcase/ui/component/ItemCard.kt
@@ -1,33 +1,38 @@
 package com.mapxus.uisdkshowcase.ui.component
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.mapxus.uisdkshowcase.R
 import com.mapxus.uisdkshowcase.model.item.InitialBounds
 import com.mapxus.uisdkshowcase.model.item.Item
 
 @Composable
 fun ItemCard(item: Item, modifier: Modifier = Modifier) {
     Card(modifier = modifier) {
-        Image(
-            modifier = Modifier.fillMaxWidth(),
-            painter = painterResource(item.photoResInt),
-            contentDescription = null,
-            contentScale = ContentScale.FillWidth
-        )
-        Column(Modifier.padding(20.dp)) {
-            Text(item.title)
-            Text(item.description)
+        Row(
+            Modifier.padding(20.dp),
+            verticalAlignment = androidx.compose.ui.Alignment.CenterVertically
+        ) {
+            Column(modifier.weight(1f)) {
+                Text(item.title, fontWeight = FontWeight.Bold)
+                Text(item.description)
+            }
+
+            Icon(
+                imageVector = Icons.AutoMirrored.Default.KeyboardArrowRight,
+                contentDescription = "Go to details",
+                modifier = Modifier.padding(start = 8.dp)
+            )
         }
     }
 }
