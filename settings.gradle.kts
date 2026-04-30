c5f1e7fb05fa54d0b18eee5778fc478453a71d35
李嘉诚 <eremli@mapxus.com>
2026-04-30 15:08:30 +0800

    整理导入的 maven 仓库

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,5 @@
 import java.net.URI
 
-val MAPXUS_RELEASE_URL: String by settings
-val MAPXUS_RELEASE_USERNAME: String by settings
-val MAPXUS_RELEASE_PASSWORD: String by settings
-val MAPXUS_SNAPSHOT_URL: String by settings
-val MAPXUS_SNAPSHOT_USERNAME: String by settings
-val MAPXUS_SNAPSHOT_PASSWORD: String by settings
-val MAPXUS_DROP_IN_UI_URL: String by settings
-
 pluginManagement {
     repositories {
         google {
@@ -26,33 +18,10 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        mavenLocal()
-        maven("https://jitpack.io")
         maven {
-            name = "Mapxus Release Repository"
-            url = URI(MAPXUS_RELEASE_URL)
+            name = "Mapxus Drop In UI Repository"
+            url = URI("https://nexus3.mapxus.com/repository/dropin-ui-android")
             isAllowInsecureProtocol = true
-            credentials {
-                username = MAPXUS_RELEASE_USERNAME
-                password = MAPXUS_RELEASE_PASSWORD
-            }
-        }
-        maven {
-            name = "Mapxus Snapshot Repository"
-            url = URI(MAPXUS_SNAPSHOT_URL)
-            isAllowInsecureProtocol = true
-            credentials {
-                username = MAPXUS_SNAPSHOT_USERNAME
-                password = MAPXUS_SNAPSHOT_PASSWORD
-            }
-        }
-        maven {
-            name = "dropin"
-            url = URI(MAPXUS_DROP_IN_UI_URL)
-            credentials {
-                username = MAPXUS_RELEASE_USERNAME
-                password = MAPXUS_RELEASE_PASSWORD
-            }
         }
     }
 }
