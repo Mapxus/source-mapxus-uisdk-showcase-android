6ca3401278d15239fd71386f737dd63093870b6c
李嘉诚 <eremli@mapxus.com>
2026-07-07 09:05:53 +0800

    build: v1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.mapxus.uisdkshowcase"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 4
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
